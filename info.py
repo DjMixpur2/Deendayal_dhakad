f953cefa983c2178ad2f9b94b1d7ac1eb072fecc
DjMixpur2 <djmixpur2@gmail.com>
2025-03-02 21:18:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -85,8 +85,8 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/CineMoj/14')  # How t
 # Link Shortener Configuration
 # ============================
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'anylinks.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'cd8ee2264345f507671a4a78b99b23580ab980de')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'urlstox.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '5bac6cb8fa7801f13d873564a08afbd9a486c09c')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/dreamcinezone')  # Tutorial video link for opening shortlink website
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 
